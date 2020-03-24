8d950a7b8df685475aa86fb191d78c01f1db69ab
Bartek <s19677@pjwstk.edu.pl>
2020-03-24 15:10:45 +0100

    Ćwiczenie 3 skończone.

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using WebApplication1.DAL;
+using System.Data.SqlClient;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Collections;
+
 
 namespace WebApplication1
 {
@@ -16,10 +21,63 @@ namespace WebApplication1
         }
 
 
-        [HttpGet]
+        [HttpGet("getStudents")]
         public IActionResult GetStudents(string orderBy)
         {
-            return Ok(_dbService.GetStudents());
+            using (SqlConnection connection = new SqlConnection("Data Source=db-mssql;Initial Catalog=s19677;Integrated Security=true"))
+            using (SqlCommand command = new SqlCommand())
+            {
+                ArrayList students = new ArrayList();
+                command.Connection = connection;
+                command.CommandText = "SELECT std.FirstName, std.LastName, std.BirthDate, sts.name, enr.Semester FROM Student std, Enrollment enr, Studies sts WHERE (std.IdEnrollment = enr.IdEnrollment) AND (enr.IdStudy = sts.IdStudy)";
+                connection.Open();
+
+                SqlDataReader dr = command.ExecuteReader();
+                while (dr.Read())
+                {
+                    Student st = new Student();
+                    st.FirstName = dr["FirstName"].ToString();
+                    st.LastName = dr["LastName"].ToString();
+                    st.BirthDate = dr["BirthDate"].ToString();
+                    st.CourseName = dr["name"].ToString();
+                    st.Semester = dr["Semester"].ToString();
+
+                    students.Add(st);
+
+                }
+                connection.Dispose();
+                string s = JsonSerializer.Serialize(students);
+                return Ok(s);
+            }
+           
+        }
+
+        // Wydzielić powtórzenia.
+        [HttpGet("getStudentInfo/{id}")]
+        public IActionResult GetStudentInfo(int id)
+        {
+            using (SqlConnection connection = new SqlConnection("Data Source=db-mssql;Initial Catalog=s19677;Integrated Security=true"))
+            using (SqlCommand command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "SELECT std.FirstName, std.LastName, std.BirthDate, sts.name, enr.Semester FROM Student std, Enrollment enr, Studies sts WHERE (std.IdEnrollment = enr.IdEnrollment) AND (enr.IdStudy = sts.IdStudy) AND std.IndexNumber ="+id+";";
+                SqlDataReader dr = command.ExecuteReader();
+                Student st = new Student();
+               
+                while (dr.Read())
+                {
+                    st.FirstName = dr["FirstName"].ToString();
+                    st.LastName = dr["LastName"].ToString();
+                    st.BirthDate = dr["BirthDate"].ToString();
+                    st.CourseName = dr["name"].ToString();
+                    st.Semester = dr["Semester"].ToString();
+                }
+
+                connection.Dispose();
+                string s = JsonSerializer.Serialize(st);
+                return Ok(s);
+            }
         }
 
         [HttpPost]
--- Models/Student.cs
+++ Models/Student.cs
@@ -1,11 +1,25 @@
-﻿namespace WebApplication1
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace WebApplication1
 {
+    [Serializable]
     public class Student
     {
         public int IdStudent { get; set; }
+        [JsonPropertyName("FirstName")]
         public string FirstName { get; set; }
+        [JsonPropertyName("LastName")]
         public string LastName { get; set; }
+        [JsonPropertyName("IndexNumber")]
         public string IndexNumber { get; set; }
+        [JsonPropertyName("BirthDate")]
+        public string BirthDate { get; set; }
+        [JsonPropertyName("CourseName")]
+        public string CourseName { get; set; }
+        [JsonPropertyName("Semester")]
+        public string Semester { get; set; }
+
 
         public Student(int IdStudent, string FirstName, string LastName)
         {
